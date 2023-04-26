e07fafe91a4b94290bacf0757cf3e0d79ca9517d
Akbal-21 <akbal153@hotmail.com>
2023-04-26 10:57:32 -0600

    Predice numeros

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,65 @@
 // import * as tf from "@tensorflow/tfjs";
 
-import { GraphModel, browser, image, loadGraphModel } from "@tensorflow/tfjs";
+import { LayersModel, browser, image, loadLayersModel } from "@tensorflow/tfjs";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import SignatureCanvas from "react-signature-canvas";
 
-// const modelUrl = "file://./model.json";
+//! si se quiere cambiar por el nuestro se tiene que invertir el comentario del modelUrl
+const modelUrl = "https://akbal-21.github.io/api_model/number/model.json";
 
-const modelUrl = "https://akbal-21.github.io/api_model/model.json";
+// const modelUrl =
+// "https://akbal-21.github.io/api_model/number_owner/model.json";
 
 export default function Home() {
-	const [model, setModel] = useState<GraphModel>();
+	//! si se quiere cambiar por el nuestro se tiene que invertir el comentario del useState de model
+	const [model, setModel] = useState<LayersModel>();
+	// const [model, setModel] = useState<GraphModel>();
 	const [imageUrl, setImageUrl] = useState("");
 	const canvasRef = useRef<SignatureCanvas>(null);
 
+	// Se cargfa el modelo
 	useEffect(() => {
-		const loadModel = async () => {
-			console.log("Cargando Modelo");
-
-			const modelCharge = await loadGraphModel(modelUrl);
-			console.log("Modelo Cargado");
-			if (modelCharge) {
-				return <>Error al cargar el modelo</>;
-			}
+		const loadmodel = async () => {
+			//! si se quiere cambiar por el nuestro se tiene que invertir el comentario del modelCharge
+			// const modelCharge = await loadGraphModel(modelUrl);
+			const modelCharge = await loadLayersModel(modelUrl);
 			setModel(modelCharge);
 		};
-		loadModel();
+		loadmodel();
 	}, []);
-
-	console.log(model);
 
 	// Funcion para predecir digitos
 	const predict = async () => {
 		if (canvasRef.current) {
 			const canvas = canvasRef.current?.getCanvas() as HTMLCanvasElement;
+
 			const img = browser.fromPixels(canvas, 1);
 			const imgResized = image.resizeBilinear(img, [28, 28]);
-			// const imgCast = cast(imgResized, "float32");
-			const tensor = imgResized.expandDims(0).toFloat().div(255);
+			const tensor = imgResized.toFloat().div(255).reshape([-1, 28, 28, 1]);
+			const tensorNeg = tensor.mul(-1).add(1);
+			// const arr = await tensorNeg.array();
 
-			const predict = model?.predict(tensor);
+			const predict = model?.predict(tensorNeg);
 			if (!predict) {
 				console.log("No hay parametros");
 				return;
 			}
 			// const predicts = Array.from(predict.toString());
-			console.log(predict);
-			// const predictedDigit = predicts.indexOf(Math.max(...prediction));
-			// setPrediction(predicts.indexOf(Math.max(...predicts)));
-			// console.log(prediction);
+			console.log("predict");
+			const predictionsArray = await predict.array(); // Convertir tensor a array
+
+			// Encontrar la clase con la probabilidad más alta
+			let maxProb = -1;
+			let maxIndex = -1;
+			for (let i = 0; i < predictionsArray[0].length; i++) {
+				console.log(`Clase ${i}: ${predictionsArray[0][i] * 100}%`);
+				if (predictionsArray[0][i] > maxProb) {
+					maxProb = predictionsArray[0][i];
+					maxIndex = i;
+				}
+			}
+			console.log("Clase identificada:", maxIndex);
 		}
 	};
 
@@ -66,6 +77,9 @@ export default function Home() {
 				<SignatureCanvas
 					ref={canvasRef}
 					backgroundColor=" #d6dbdf "
+					dotSize={8}
+					minWidth={8}
+					maxWidth={8}
 					canvasProps={{ width: 200, height: 200 }}
 				/>
 			</div>
